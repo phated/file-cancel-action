d66c427619f8ee051387ea90deb37fd4549cff72
Blaine Bublitz <blaine.bublitz@gmail.com>
2021-05-05 16:48:54 -0700

    feat: Initial implementation

--- index.js
+++ index.js
@@ -1,18 +1,58 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@action/github');
+const glob = require('@actions/glob');
 
-
-// most @actions toolkit packages have async methods
 async function run() {
+  const token = core.getInput('token', { required: true });
+  let exists = core.getInput('exists');
+  let notExists = core.getInput('not-exists');
+
+  const octokit = github.getOctokit(token);
+
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    if (!exists && !notExists) {
+      throw new Error('Either `exists` or `not-exists` input is required.');
+    }
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    let existsGlobber;
+    if (exists) {
+      exists = JSON.parse(exists);
+      if (Array.isArray(exists)) {
+        exists = exists.join('\n');
+      }
+      if (typeof exists !== 'string') {
+        throw new Error('The value of `exists` must be a string or array.');
+      }
+      existsGlobber = await glob.create(exists);
+    }
 
-    core.setOutput('time', new Date().toTimeString());
+    const existsFiles = await existsGlobber.glob();
+    if (existsFiles.length > 0) {
+      await octokit.rest.actions.cancelWorkflowRun({
+        ...github.context.repo,
+        run_id: github.context.runId
+      });
+    }
+
+    let notExistsGlobber;
+    if (notExists) {
+      notExists = JSON.parse(notExists);
+      if (Array.isArray(notExists)) {
+        notExists = notExists.join('\n');
+      }
+      if (typeof notExists !== 'string') {
+        throw new Error('The value of `not-exists` must be a string or array.');
+      }
+      notExistsGlobber = await glob.create(notExists);
+    }
+
+    const notExistsFiles = await notExistsGlobber.glob();
+    if (notExistsFiles.length === 0) {
+      await octokit.rest.actions.cancelWorkflowRun({
+        ...github.context.repo,
+        run_id: github.context.runId
+      });
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
